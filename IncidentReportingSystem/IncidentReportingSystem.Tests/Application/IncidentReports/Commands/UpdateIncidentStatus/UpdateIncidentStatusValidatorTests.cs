5f2828f7d47b57258d2f56b321fc5f49655e9b88
Guy Sne <guysneh@gmail.com>
2025-08-05 01:31:43 +0200

    add unit tests

---
+++ IncidentReportingSystem/IncidentReportingSystem.Tests/Application/IncidentReports/Commands/UpdateIncidentStatus/UpdateIncidentStatusValidatorTests.cs
@@ -0,0 +1,57 @@
+﻿using FluentValidation.TestHelper;
+using IncidentReportingSystem.Application.IncidentReports.Commands.UpdateIncidentStatus;
+using IncidentReportingSystem.Application.IncidentReports.Validators;
+using IncidentReportingSystem.Domain.Enums;
+using Xunit;
+
+namespace IncidentReportingSystem.Tests.Application.IncidentReports.Commands.CreateIncidentReport;
+
+public class UpdateIncidentStatusValidatorTests
+{
+    private readonly UpdateIncidentStatusValidator _validator;
+
+    public UpdateIncidentStatusValidatorTests()
+    {
+        _validator = new UpdateIncidentStatusValidator();
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_Id_Is_Empty()
+    {
+        // Arrange
+        var command = new UpdateIncidentStatusCommand(Guid.Empty, IncidentStatus.Closed);
+
+        // Act
+        var result = _validator.TestValidate(command);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Id);
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_Status_Is_Not_Enum()
+    {
+        // Arrange
+        var invalidStatus = (IncidentStatus)999; // deliberately out of range
+        var command = new UpdateIncidentStatusCommand(Guid.NewGuid(), invalidStatus);
+
+        // Act
+        var result = _validator.TestValidate(command);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.NewStatus);
+    }
+
+    [Fact]
+    public void Should_Not_Have_Any_Errors_For_Valid_Command()
+    {
+        // Arrange
+        var command = new UpdateIncidentStatusCommand(Guid.NewGuid(), IncidentStatus.Open);
+
+        // Act
+        var result = _validator.TestValidate(command);
+
+        // Assert
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
