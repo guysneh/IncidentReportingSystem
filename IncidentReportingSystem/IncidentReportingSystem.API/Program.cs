44d9cc22f812963baa3cfadc5f99d6738341e90c
Guy Sne <guysneh@gmail.com>
2025-08-04 11:03:19 +0200

    Implement IncidentReportRepository with in-memory ApplicationDbContext

--- IncidentReportingSystem/IncidentReportingSystem.API/Program.cs
+++ IncidentReportingSystem/IncidentReportingSystem.API/Program.cs
@@ -1,7 +1,11 @@
 using FluentValidation;
 using IncidentReportingSystem.Application.Common.Behaviors;
 using IncidentReportingSystem.Application.IncidentReports.Commands.CreateIncidentReport;
+using IncidentReportingSystem.Domain.Interfaces;
+using IncidentReportingSystem.Infrastructure.IncidentReports.Repositories;
+using IncidentReportingSystem.Infrastructure.Persistence;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +16,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateIncidentReportCommandHandler).Assembly));
 builder.Services.AddValidatorsFromAssembly(typeof(CreateIncidentReportCommandValidator).Assembly);
 builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
-
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseInMemoryDatabase("IncidentReportsDb"));
+builder.Services.AddScoped<IIncidentReportRepository, IncidentReportRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
