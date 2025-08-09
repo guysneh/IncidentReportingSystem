f32d024081aaea0a1e36bde249101d1c6cee8119
Guy Sne <guysneh@gmail.com>
2025-08-09 17:24:06 +0200

    fix pipeline and some improvements for migrations

---
+++ IncidentReportingSystem/IncidentReportingSystem.Infrastructure/DesignTimeDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using IncidentReportingSystem.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+
+namespace IncidentReportingSystem.Infrastructure
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var conn = Environment.GetEnvironmentVariable("ConnectionStrings__Default");
+            if (string.IsNullOrWhiteSpace(conn))
+                throw new InvalidOperationException("ConnectionStrings__Default is not set.");
+
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseNpgsql(conn)
+                .Options;
+
+            return (ApplicationDbContext)Activator.CreateInstance(typeof(ApplicationDbContext), options)!;
+        }
+    }
+}
