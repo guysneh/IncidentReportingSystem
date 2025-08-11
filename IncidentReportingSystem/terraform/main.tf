fd0a7773c7ac0945991f4e26ba47882711346c3c
Guy Sne <guysneh@gmail.com>
2025-08-11 17:23:36 +0200

    terraform(app): use module output for Web App MI principal_id; remove redundant data source

--- IncidentReportingSystem/terraform/main.tf
+++ IncidentReportingSystem/terraform/main.tf
@@ -16,16 +16,16 @@ module "app_service_plan" {
 }
 
 module "postgres" {
-  source                 = "./modules/postgres"
-  postgresql_server_name = "incident-db"
-  location               = var.location
-  resource_group_name    = module.resource_group.name
-  db_admin_username      = var.db_admin_username
-  db_admin_password      = var.db_admin_password
-  tags                   = var.default_tags
-
+  source                        = "./modules/postgres"
+  postgresql_server_name        = "incident-db"
+  location                      = var.location
+  resource_group_name           = module.resource_group.name
+  db_admin_username             = var.db_admin_username
+  db_admin_password             = random_password.postgres_admin.result
+  tags                          = var.default_tags
   public_network_access_enabled = true
 }
+
 
 
 module "key_vault" {
@@ -36,13 +36,14 @@ module "key_vault" {
   tags                = var.default_tags
 
   secrets = {
-    PostgreSqlConnectionString = var.postgres_connection_string
+    PostgreSqlConnectionString = "Host=${module.postgres.fqdn};Database=postgres;Username=${var.db_admin_username};Password=${random_password.postgres_admin.result};Port=5432;Ssl Mode=Require;Trust Server Certificate=true"
     jwt-issuer                 = var.jwt_issuer
     jwt-audience               = var.jwt_audience
     jwt-secret                 = random_password.jwt_secret.result
     jwt-expiry-minutes         = tostring(var.jwt_expiry_minutes)
   }
 }
+
 
 module "app_service" {
   source              = "./modules/app_service"
@@ -53,27 +54,19 @@ module "app_service" {
   tags                = var.default_tags
 
   app_settings = {
-    "ConnectionStrings__Default" = "@Microsoft.KeyVault(SecretUri=${module.key_vault.uri}secrets/PostgreSqlConnectionString/)"
-    "ASPNETCORE_ENVIRONMENT"     = "Production"
-
-    "Jwt__Issuer"        = var.jwt_issuer
-    "Jwt__Audience"      = var.jwt_audience
-    "Jwt__ExpiryMinutes" = tostring(var.jwt_expiry_minutes)
-
-    "Jwt__Secret"   = "@Microsoft.KeyVault(SecretUri=${module.key_vault.uri}secrets/jwt-secret/)"
-    "EnableSwagger" = "true"
+    "ConnectionStrings__DefaultConnection" = "Host=${module.postgres.fqdn};Database=postgres;Username=${var.db_admin_username};Password=${random_password.postgres_admin.result};Port=5432;Ssl Mode=Require;Trust Server Certificate=true"
+    "ASPNETCORE_ENVIRONMENT"               = "Production"
+    "Jwt__Issuer"                          = "@Microsoft.KeyVault(SecretUri=${module.key_vault.uri}secrets/jwt-issuer/)"
+    "Jwt__Audience"                        = "@Microsoft.KeyVault(SecretUri=${module.key_vault.uri}secrets/jwt-audience/)"
+    "Jwt__Secret"                          = "@Microsoft.KeyVault(SecretUri=${module.key_vault.uri}secrets/jwt-secret/)"
+    "Jwt__ExpiryMinutes"                   = tostring(var.jwt_expiry_minutes)
+    "EnableSwagger"                        = "true"
   }
   always_on = true
-}
-
-data "azurerm_linux_web_app" "app_identity" {
-  name                = var.app_service_name
-  resource_group_name = module.resource_group.name
-  depends_on          = [module.app_service]
 }
 
 resource "azurerm_role_assignment" "webapp_kv_secrets_user" {
   scope                = module.key_vault.id
   role_definition_name = "Key Vault Secrets User"
-  principal_id         = data.azurerm_linux_web_app.app_identity.identity[0].principal_id
+  principal_id         = module.app_service.principal_id
 }
