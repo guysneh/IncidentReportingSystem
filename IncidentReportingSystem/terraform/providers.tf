9e641e5839db77df87b3e3b412935d9a6e1ff491
Guy Sne <guysneh@gmail.com>
2025-08-17 23:11:32 +0200

    fix soft delete issue

--- IncidentReportingSystem/terraform/providers.tf
+++ IncidentReportingSystem/terraform/providers.tf
@@ -22,10 +22,17 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    app_configuration {
+      # Disable soft-deleted pre-check/recovery. We handle purge ourselves if needed.
+      recover_soft_deleted = false
+    }
+  }
+
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
 }
+
 
 provider "azuread" {
   tenant_id = var.tenant_id
