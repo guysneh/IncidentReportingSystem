21630fdf792e3a0d598fb75bfe1d8de278257333
Guy Sne <guysneh@gmail.com>
2025-08-18 00:20:57 +0200

    remove temp file

--- IncidentReportingSystem/terraform/imports_appcfg.tf
+++
@@ -1,4 +0,0 @@
-import {
-  to = module.app_configuration.azurerm_app_configuration.appcfg
-  id = "/subscriptions/27406235-5e35-4236-9a1a-054b917fb75c/resourceGroups/incident-rg/providers/Microsoft.AppConfiguration/configurationStores/incident-appcfg"
-}
