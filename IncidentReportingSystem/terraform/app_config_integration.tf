934f67ffd9361ed788aa1897344f0e3ba62053c3
Guy Sne <guysneh@gmail.com>
2025-08-20 02:28:11 +0200

    change sample ratio value

--- IncidentReportingSystem/terraform/app_config_integration.tf
+++ IncidentReportingSystem/terraform/app_config_integration.tf
@@ -34,7 +34,7 @@ resource "azurerm_app_configuration_key" "sample_ratio" {
   configuration_store_id = data.azurerm_app_configuration.this.id
   key                    = "MyAppSettings:SampleRatio"
   label                  = var.app_config_label
-  value                  = "1.0"
+  value                  = var.sample_ratio
   content_type           = "text/plain"
   tags                   = var.default_tags
 }
