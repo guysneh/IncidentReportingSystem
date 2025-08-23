d0093cc6419f8af8fc50d7683b3d9218bbadae52
Guy Sne <guysneh@gmail.com>
2025-08-23 21:35:51 +0200

    raise code coverage

---
+++ IncidentReportingSystem/IncidentReportingSystem.Tests/API/Controllers/AuthControllerTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
+
+using IncidentReportingSystem.API.Contracts.Authentication;
+using IncidentReportingSystem.API.Controllers;
+using IncidentReportingSystem.Application.Exceptions;
+using IncidentReportingSystem.Application.Features.Users.Commands.LoginUser;
+using IncidentReportingSystem.Application.Features.Users.Commands.RegisterUser;
+
+namespace IncidentReportingSystem.Tests.API.Controllers
+{
+    [Trait("Category", "Unit")]
+    public sealed class AuthControllerTests
+    {
+        private static AuthController NewController(Mock<IMediator>? m = null)
+            => new AuthController((m ?? new Mock<IMediator>()).Object);
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public async Task Login_Unauthorized_On_InvalidCredentials()
+        {
+            var m = new Mock<IMediator>(MockBehavior.Strict);
+            m.Setup(x => x.Send(It.IsAny<LoginUserCommand>(), It.IsAny<CancellationToken>()))
+             .ThrowsAsync(new InvalidCredentialsException());
+
+            var ctrl = NewController(m);
+            var req = new LoginRequest { Email = "a@b.com", Password = "x" };
+
+            var res = await ctrl.Login(req, CancellationToken.None);
+
+            res.Result.Should().BeOfType<UnauthorizedObjectResult>();
+            m.VerifyAll();
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public async Task Register_Conflict_On_EmailAlreadyExists()
+        {
+            var m = new Mock<IMediator>(MockBehavior.Strict);
+            m.Setup(x => x.Send(It.IsAny<RegisterUserCommand>(), It.IsAny<CancellationToken>()))
+             .ThrowsAsync(new EmailAlreadyExistsException("a@b.com"));
+
+            var ctrl = NewController(m);
+            var cmd = new RegisterUserCommand("a@b.com", "P@ssw0rd!", new[] { "Operator" });
+
+            var res = await ctrl.Register(cmd, CancellationToken.None);
+
+            res.Should().BeOfType<ConflictObjectResult>();
+            m.VerifyAll();
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public async Task Register_ServiceUnavailable_On_DbUpdateException()
+        {
+            var m = new Mock<IMediator>(MockBehavior.Strict);
+            m.Setup(x => x.Send(It.IsAny<RegisterUserCommand>(), It.IsAny<CancellationToken>()))
+             .ThrowsAsync(new DbUpdateException("db err", (Exception?)null));
+
+            var ctrl = NewController(m);
+            var cmd = new RegisterUserCommand("a@b.com", "P@ssw0rd!", new[] { "Operator" });
+
+            var res = await ctrl.Register(cmd, CancellationToken.None);
+
+            res.Should().BeOfType<Microsoft.AspNetCore.Mvc.ConflictObjectResult>()
+               .Which.StatusCode.Should().Be(503);
+            m.VerifyAll();
+        }
+
+        [Fact]
+        [Trait("Category", "Unit")]
+        public async Task Register_BadRequest_On_ArgumentOrValidation()
+        {
+            var m = new Mock<IMediator>(MockBehavior.Strict);
+            m.Setup(x => x.Send(It.IsAny<RegisterUserCommand>(), It.IsAny<CancellationToken>()))
+             .ThrowsAsync(new ArgumentException("bad"));
+
+            var ctrl = NewController(m);
+            var cmd = new RegisterUserCommand("bad", "123", Array.Empty<string>());
+
+            var res = await ctrl.Register(cmd, CancellationToken.None);
+
+            res.Should().BeOfType<BadRequestObjectResult>();
+            m.VerifyAll();
+        }
+    }
+}
---
+++ IncidentReportingSystem/IncidentReportingSystem.Tests/API/Controllers/IncidentStatisticsControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+using IncidentReportingSystem.API.Controllers;
+using IncidentReportingSystem.Application.Features.IncidentReports.Dtos;
+using IncidentReportingSystem.Application.Features.IncidentReports.Queries.GetIncidentStatistics;
+
+namespace IncidentReportingSystem.Tests.API.Controllers
+{
+    [Trait("Category", "Unit")]
+    public sealed class IncidentStatisticsControllerTests
+    {
+        [Fact]
+        [Trait("Category", "Unit")]
+        public async Task GetStatistics_Returns_Ok_With_Dto()
+        {
+            var dto = new IncidentStatisticsDto { TotalIncidents = 42 };
+            var m = new Mock<IMediator>(MockBehavior.Strict);
+            m.Setup(x => x.Send(It.IsAny<GetIncidentStatisticsQuery>(), It.IsAny<CancellationToken>()))
+             .ReturnsAsync(dto);
+
+            var ctrl = new IncidentStatisticsController(m.Object);
+            var res = await ctrl.GetStatistics(CancellationToken.None);
+
+            res.Result.Should().BeOfType<OkObjectResult>();
+            (res.Result as OkObjectResult)!.Value.Should().Be(dto);
+            m.VerifyAll();
+        }
+    }
+}
