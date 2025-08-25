67ef5f28e61a7c5bd1f5aaa6ff73f4de45831b98
Guy Sne <guysneh@gmail.com>
2025-08-25 10:28:57 +0200

    add namespace

--- IncidentReportingSystem/IncidentReportingSystem.API/Extensions/HealthAndLimitsExtensions.cs
+++ IncidentReportingSystem/IncidentReportingSystem.API/Extensions/HealthAndLimitsExtensions.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 
+namespace IncidentReportingSystem.API.Extensions;
 public static class HealthAndRateLimitingExtensions
 {
     public static IServiceCollection AddHealthAndRateLimiting(
