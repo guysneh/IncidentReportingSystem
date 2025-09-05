6a9bcb749b041365defa046e923d5a60a8d2cade
Guy Sne <guysneh@gmail.com>
2025-09-05 21:50:22 +0200

    modify design to be mobile responsive

--- UI/IncidentReportingSystem.UI/wwwroot/lib/js/irs-charts.js
+++ UI/IncidentReportingSystem.UI/wwwroot/lib/js/irs-charts.js
@@ -3,16 +3,28 @@
         var cv = document.getElementById(id);
         if (!cv || typeof Chart === "undefined") return;
         var ctx = cv.getContext('2d');
+
+        var isSmall = window.matchMedia('(max-width: 576px)').matches;
+        var opts = {
+            responsive: true,
+            maintainAspectRatio: false, // we control size via CSS frame
+            plugins: {
+                legend: { position: isSmall ? 'bottom' : 'right', labels: { boxWidth: 10 } },
+                tooltip: { enabled: true }
+            },
+            layout: { padding: 0 }
+        };
+
         if (!cv._chart) {
             cv._chart = new Chart(ctx, {
                 type: 'pie',
-                data: { labels: labels, datasets: [{ data: data }] },
-                options: { responsive: true, maintainAspectRatio: false }
+                data: { labels: labels || [], datasets: [{ data: data || [] }] },
+                options: opts
             });
         } else {
             cv._chart.data.labels = labels || [];
-            var ds = cv._chart.data.datasets[0];
-            ds.data = data || [];
+            cv._chart.data.datasets[0].data = data || [];
+            cv._chart.options = opts;
             cv._chart.update();
         }
     },
